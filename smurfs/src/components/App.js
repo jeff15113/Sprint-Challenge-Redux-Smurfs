88500ca9898ff8a076620fa7bbfbbf010c6bd786
jeff15113 <itsmejeffery@gmail.com>
2019-02-01 14:08:28 -0500

    added fetchsmurf, added addsmurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+import "./App.css";
+import SmurfForm from "./SmurfForm";
+import Smurfs from "./Smurfs";
+import { connect } from "react-redux";
+import { fetchsmurfs, addSmurf } from "../actions";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +12,57 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: []
+    };
+  }
+  componentDidMount() {
+    console.log("mounted");
+    this.props.fetchsmurfs();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <header className="headerbar">
+          <h1>**SMURF**</h1>
+          <div className="nav">
+            <NavLink to="/">Smurf List</NavLink>
+            <NavLink to="/smurf-form">Add Smurf</NavLink>
+          </div>
+        </header>
+
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm {...props} addSmurf={this.props.addSmurf} />
+          )}
+        />
+        {this.props.fetchingSmurfs ? (
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Smurfs
+                getSmurfList={this.props.fetchsmurfs}
+                smurfs={this.props.smurfs}
+              />
+            )}
+          />
+        ) : null}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchsmurfs, addSmurf }
+)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,44 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESSFUL,
+  FETCH_SMURF_FAIL,
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESSFUL,
+  ADD_SMURF_FAIL
+} from "../actions";
+
+const initialstate = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
+
+export default (state = initialstate, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return { ...state, fetchingSmurfs: true, error: null };
+
+    case FETCH_SMURF_SUCCESSFUL:
+      return { ...state, smurfs: action.payload, fetchingSmurfs: false };
+
+    case FETCH_SMURF_FAIL:
+      return { ...state, fetchingSmurfs: false, error: action.payload };
+
+    case ADD_SMURF_START:
+      return { ...state, addingSmurf: true, error: null };
+
+    case ADD_SMURF_SUCCESSFUL:
+      return { ...state, smurfs: action.payload, fetchingSmurfs: false };
+
+    case ADD_SMURF_FAIL:
+      return { ...state, fetchingSmurfs: false, error: action.payload };
+    default:
+      return state;
+  }
+};
